3ab7ee864e46fb98ac71903f97ad06f67a15af55
ram <ramazanovmadiar829@gmail.com>
2026-02-07 08:32:56 +0500

    .

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -13,10 +13,4 @@ public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-
-    public static void closeConnection(Connection conn) {
-        try {
-            if (conn != null) conn.close();
-        } catch (SQLException ignored) {}
-    }
 }
